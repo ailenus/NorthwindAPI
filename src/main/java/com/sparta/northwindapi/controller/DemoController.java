bb62b6c864790233e3968e8480ff7277640dec7c
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-10-05 15:20:24 +0100

    commit controller

--- src/main/java/com/sparta/northwindapi/controller/DemoController.java
+++ src/main/java/com/sparta/northwindapi/controller/DemoController.java
@@ -1,4 +1,30 @@
 package com.sparta.northwindapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/")
+    public String basic() {
+        return String.format("""
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Northwind API</title>
+                        <link rel="icon" type="image/x-icon" href="https://i.pinimg.com/originals/03/ed/de/03edde789384ab169b248e0b37c96d47.jpg">
+                        <style>
+                        .right { text-align:right; }
+                        .center { text-align:center; }
+                        </style>
+                    </head>
+                    <body>
+                        <h1 class="center">
+                            Welcome to the Northwind API.
+                        </h1>
+                    </body>
+                </html>
+                """);
+    }
 }

e95032182df1a38c57d077a633dce7f96c132ed5
ailenus <ailenus@users.noreply.github.com>
2022-10-05 14:35:31 +0100

    initial commit

---
+++ src/test/java/com/spartaglobal/northwindapi/NorthwindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.northwindapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

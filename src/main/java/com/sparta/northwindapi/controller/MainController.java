99abc6408510554769de3390cf95d682b788b9ed
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-10-05 16:18:55 +0100

    commit path stuff

--- src/main/java/com/sparta/northwindapi/controller/MainController.java
+++ src/main/java/com/sparta/northwindapi/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.sparta.northwindapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("")
     public String basic() {
         return """
                 <!DOCTYPE html>

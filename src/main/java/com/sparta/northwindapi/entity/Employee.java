08ff906a30ea4ed17c6e2830827c525a9d8e818d
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-10-06 16:09:05 +0100

    database relation updates

--- src/main/java/com/sparta/northwindapi/entity/Employee.java
+++ src/main/java/com/sparta/northwindapi/entity/Employee.java
@@ -59,18 +59,11 @@ public class Employee {
     @Column(name = "Notes", nullable = false)
     private String notes;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ReportsTo")
-    private Employee reportsTo;
-
     @Column(name = "PhotoPath")
     private String photoPath;
 
     @Column(name = "Salary")
     private Float salary;
-
-    @OneToMany(mappedBy = "employeeID")
-    private Set<Order> orders = new LinkedHashSet<>();
 
     @ManyToMany
     @JoinTable(name = "EmployeeTerritories",
@@ -206,14 +199,6 @@ public class Employee {
         this.notes = notes;
     }
 
-    public Employee getReportsTo() {
-        return reportsTo;
-    }
-
-    public void setReportsTo(Employee reportsTo) {
-        this.reportsTo = reportsTo;
-    }
-
     public String getPhotoPath() {
         return photoPath;
     }
@@ -228,14 +213,6 @@ public class Employee {
 
     public void setSalary(Float salary) {
         this.salary = salary;
-    }
-
-    public Set<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(Set<Order> orders) {
-        this.orders = orders;
     }
 
     public Set<Territory> getTerritories() {
